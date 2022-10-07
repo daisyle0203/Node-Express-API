7bec1384605c3e5fda31aad4817406f059a81d84
Daisy Le <daisy.le0203@gmail.com>
2022-10-07 18:13:33 -0500

    moved () to controllers folder for cleaner code

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,18 @@
 import express from "express"
-import { v4 as uuidv4 } from "uuid"
 
-import { createUser, getUser } from "../controllers/users"
+import { createUser, getUsers, getUser, deleteUser, updateUser } from "../controllers/users.js"
 
 const router = express.Router()
 
-let users = []
-
 // All routes in here are stating with /users
-router.get("/", getUser)
+router.get("/", getUsers)
 
 router.post("/", createUser)
 
-router.get("/:id", (req, res) => {
-  const { id } = req.params
+router.get("/:id", getUser)
 
-  const foundUser = users.find((user) => user.id === id)
+router.delete("/:id", deleteUser)
 
-  res.send(foundUser)
-})
-
-router.delete("/:id", (req, res) => {
-  const { id } = req.params
-
-  users = users.filter((user) => user.id !== id)
-
-  res.send(`User with the id ${id} deleted from the database.`)
-})
-
-router.patch("/:id", (req, res) => {
-  const { id } = req.params
-  const { firstName, lastName, age } = req.body
-
-  const user = users.find((user) => user.id === id)
-
-  if (firstName) user.firstName = firstName
-
-  if (lastName) user.lastName = lastName
-
-  if (age) user.age = age
-
-  res.send(`User with the id ${id} has been updated.`)
-})
+router.patch("/:id", updateUser)
 
 export default router
